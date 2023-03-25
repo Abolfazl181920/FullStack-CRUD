03bad11d8b39e97b7fdd94e272850471570a754a
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-25 04:00:31 -0700

    [ Style / Client ]: Got data and mapped on to Books page add styles and so on!

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Books = () => {
+
+  const [ booksList, setBooksList ] = useState([])
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/books`).then(res => {
+      setBooksList(res.data)
+    })
+  }, [ booksList ])
+  
   return (
-    <div>Books</div>
+    <div className='flex__container'>
+      {
+        booksList.map(book => (
+          <section className='book__list' key={book.id}>
+            <h4> {book.title} </h4>
+            <p className='book__desc'> {book.descr} </p>
+            <span> {book.price}$ </span>
+          </section>
+        ))
+      }
+    </div>
   )
 }
 
