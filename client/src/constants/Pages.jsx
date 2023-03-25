d05de406fc13b73e3f37daf98e5cc84879ebc4b1
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-25 12:21:16 -0700

    [Constants]: import Update component to constants!!

--- client/src/constants/Pages.jsx
+++ client/src/constants/Pages.jsx
@@ -1,11 +1,13 @@
 import Home from '../pages/Home'
 import Books from '../pages/Books'
 import Add from '../pages/Add'
+import Update from '../pages/Update'
 import NotFound from '../pages/NotFound'
 
 export default {
     Home,
     Books,
     Add,
+    Update,
     NotFound
 }
