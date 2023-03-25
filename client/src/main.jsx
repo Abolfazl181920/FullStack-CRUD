095ffcc9936f91d393deb771fc047a51a29259c2
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-25 03:26:54 -0700

    [ client ]: Generate client basic suffts!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import AllRoutes from './routes/AllRoutes'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <AllRoutes />
 )
