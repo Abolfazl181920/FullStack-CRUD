095fd0bfc9dc56fbbf2e1f0ea7eb81df203eb527
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-25 12:59:59 -0700

    [ Update ]: Create put method to update(withBug)!

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,8 +1,13 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useParams, Location } from 'react-router-dom'
 import * as Yup from 'yup'
+import axios from 'axios'
 
 const Update = () => {
 
+    // const bookId = useParams()
+    const bookId = location.pathname.split("/")[2]
+    
     const initialValues = {
         title: "",
         descr: "",
@@ -14,6 +19,14 @@ const Update = () => {
         descr: Yup.string(),
         price: Yup.number()
     })
+
+    const updateBook = () => {
+        try {
+          axios.put(`http://localhost:3001/books/` + bookId, data)
+        } catch {
+          console.error(`Failed to update a book with this id: ${bookId}`)
+        }
+    }
 
     return(
         <div>
@@ -43,7 +56,7 @@ const Update = () => {
                         placeholder="(Ex. John123...)"
                     />
 
-                    <button type="submit"> Update Book</button>
+                    <button type="submit" onClick={updateBook}> Update Book</button>
                 </Form>
             </Formik>
         </div>
